5935dad4f7f11276c462431c64a5e490949adc84
pflastpr-rpg <pflastpr@gmail.com>
2026-05-22 21:50:53 -0500

    Add files via upload

---
+++ bestiary-art/rename_final.bat.bat
@@ -0,0 +1,105 @@
+@echo off
+
+echo Blood and Purpose - Final Rename to Clean Hyphen Format
+
+cd /d "%~dp0"
+
+echo.
+
+if exist "aethon__the_world_breaker.png" ren "aethon__the_world_breaker.png" "aethon-the-world-breaker.png"
+
+if exist "air_elemental__the_gale_wisp__lesser_.png" ren "air_elemental__the_gale_wisp__lesser_.png" "air-elemental-the-gale-wisp-lesser.png"
+
+if exist "boar__razorgor___the_living_battering_ram.png" ren "boar__razorgor___the_living_battering_ram.png" "boar-razorgor-the-living-battering-ram.png"
+
+if exist "bog_octopuss__silt_lurker_.png" ren "bog_octopuss__silt_lurker_.png" "bog-octopuss-silt-lurker.png"
+
+if exist "bog_witch__swamp_hag_.png" ren "bog_witch__swamp_hag_.png" "bog-witch-swamp-hag.png"
+
+if exist "chaos_dwarf__the_lava_forged__bin_uluhm_taskmaster_.png" ren "chaos_dwarf__the_lava_forged__bin_uluhm_taskmaster_.png" "chaos-dwarf-the-lava-forged-bin-uluhm-taskmaster.png"
+
+if exist "chaos_wizard__warlock_.png" ren "chaos_wizard__warlock_.png" "chaos-wizard-warlock.png"
+
+if exist "chromatic_dragon__the_sovereign__the_five_fold_ruin_.png" ren "chromatic_dragon__the_sovereign__the_five_fold_ruin_.png" "chromatic-dragon-the-sovereign-the-five-fold-ruin.png"
+
+if exist "deer_stag___ghost_of_the_deep_woods.png" ren "deer_stag___ghost_of_the_deep_woods.png" "deer-stag-ghost-of-the-deep-woods.png"
+
+if exist "dog___war__the_loyal_anchor.png" ren "dog___war__the_loyal_anchor.png" "dog-war-the-loyal-anchor.png"
+
+if exist "dwarf__kel_thurum_militiaman___iron_joe__.png" ren "dwarf__kel_thurum_militiaman___iron_joe__.png" "dwarf-kel-thurum-militiaman-iron-joe.png"
+
+if exist "dwarf__the_living_stone__kel_thurum_defender_.png" ren "dwarf__the_living_stone__kel_thurum_defender_.png" "dwarf-the-living-stone-kel-thurum-defender.png"
+
+if exist "earth_elemental__greater_the_mountain_heart.png" ren "earth_elemental__greater_the_mountain_heart.png" "earth-elemental-greater-the-mountain-heart.png"
+
+if exist "earth_elemental__lesser__the_ruble_clod.png" ren "earth_elemental__lesser__the_ruble_clod.png" "earth-elemental-lesser-the-ruble-clod.png"
+
+if exist "fey_nymph__the_grove_warden.png" ren "fey_nymph__the_grove_warden.png" "fey-nymph-the-grove-warden.png"
+
+if exist "ghast__the_bone_snapper_.png" ren "ghast__the_bone_snapper_.png" "ghast-the-bone-snapper.png"
+
+if exist "ghoul__the_grave_feaster_.png" ren "ghoul__the_grave_feaster_.png" "ghoul-the-grave-feaster.png"
+
+if exist "gnoll__the_scavenger_of_the_wastes.png" ren "gnoll__the_scavenger_of_the_wastes.png" "gnoll-the-scavenger-of-the-wastes.png"
+
+if exist "grabzug_the_skull_crusher_.png" ren "grabzug_the_skull_crusher_.png" "grabzug-the-skull-crusher.png"
+
+if exist "harpy__siren_of_the_canyons.png" ren "harpy__siren_of_the_canyons.png" "harpy-siren-of-the-canyons.png"
+
+if exist "knight_of_the_ruined_order__dread_knight_.png" ren "knight_of_the_ruined_order__dread_knight_.png" "knight-of-the-ruined-order-dread-knight.png"
+
+if exist "kobold__the_warren_menace.png" ren "kobold__the_warren_menace.png" "kobold-the-warren-menace.png"
+
+if exist "kraken__the_abyssal_titan.png" ren "kraken__the_abyssal_titan.png" "kraken-the-abyssal-titan.png"
+
+if exist "magma_caller__the_vein_witch_.png" ren "magma_caller__the_vein_witch_.png" "magma-caller-the-vein-witch.png"
+
+if exist "malakor__the_monarch_of_midnight.png" ren "malakor__the_monarch_of_midnight.png" "malakor-the-monarch-of-midnight.png"
+
+if exist "medusa__the_gorgon_queen.png" ren "medusa__the_gorgon_queen.png" "medusa-the-gorgon-queen.png"
+
+if exist "midnight_hound__shadow_stalker_.png" ren "midnight_hound__shadow_stalker_.png" "midnight-hound-shadow-stalker.png"
+
+if exist "mimic__the_false_object.png" ren "mimic__the_false_object.png" "mimic-the-false-object.png"
+
+if exist "moss_revenant.png" ren "moss_revenant.png" "moss-revenant.png"
+
+if exist "oracle_of_third_eye__mystic_.png" ren "oracle_of_third_eye__mystic_.png" "oracle-of-third-eye-mystic.png"
+
+if exist "orc__the_green_tide_vanguard.png" ren "orc__the_green_tide_vanguard.png" "orc-the-green-tide-vanguard.png"
+
+if exist "owlbear__the_silent_ravager.png" ren "owlbear__the_silent_ravager.png" "owlbear-the-silent-ravager.png"
+
+if exist "pit_fiend__general_of_the_damned.png" ren "pit_fiend__general_of_the_damned.png" "pit-fiend-general-of-the-damned.png"
+
+if exist "pit_hulk__underworld_gladiator.png" ren "pit_hulk__underworld_gladiator.png" "pit-hulk-underworld-gladiator.png"
+
+if exist "pyreborn_kin_sun_forged_.png" ren "pyreborn_kin_sun_forged_.png" "pyreborn-kin-sun-forged.png"
+
+if exist "seraphim__the_six_winged_arbiter.png" ren "seraphim__the_six_winged_arbiter.png" "seraphim-the-six-winged-arbiter.png"
+
+if exist "shade_crow__the_hollow_gourd.png" ren "shade_crow__the_hollow_gourd.png" "shade-crow-the-hollow-gourd.png"
+
+if exist "skeleton__lesser_undead_.png" ren "skeleton__lesser_undead_.png" "skeleton-lesser-undead.png"
+
+if exist "triad_weaver__arcane_.png" ren "triad_weaver__arcane_.png" "triad-weaver-arcane.png"
+
+if exist "umbra_witch__the_void_weaver.png" ren "umbra_witch__the_void_weaver.png" "umbra-witch-the-void-weaver.png"
+
+if exist "umbric_flayer__shadow_stalker_.png" ren "umbric_flayer__shadow_stalker_.png" "umbric-flayer-shadow-stalker.png"
+
+if exist "vulkan_kahn__the_first_forge_.png" ren "vulkan_kahn__the_first_forge_.png" "vulkan-kahn-the-first-forge.png"
+
+if exist "warrior_chaos__iron_bound_.png" ren "warrior_chaos__iron_bound_.png" "warrior-chaos-iron-bound.png"
+
+if exist "warrior_of_chaos__champion_.png" ren "warrior_of_chaos__champion_.png" "warrior-of-chaos-champion.png"
+
+if exist "wither_walker__the_blighted_sylvan.png" ren "wither_walker__the_blighted_sylvan.png" "wither-walker-the-blighted-sylvan.png"
+
+if exist "zephyr__greater_hurricane_core.png" ren "zephyr__greater_hurricane_core.png" "zephyr-greater-hurricane-core.png"
+
+echo.
+
+echo Done!
+
+pause
